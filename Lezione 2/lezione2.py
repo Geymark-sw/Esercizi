1aec9e9c6de8076d56243fc3fab31048f34afbee
Geymark-sw <emmanuelpapio12@gmail.com>
2024-04-18 13:03:31 +0200

    finito esrcizio 3 (2-5)

--- Lezione 2/lezione2.py
+++ Lezione 2/lezione2.py
@@ -14,3 +14,12 @@ print(message)
 """2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case."""
 
 print(f"{name}, {name.upper()}, {name.lower()}")
+
+"""2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks: 
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”"""
+
+autor: str = "David Goggins"
+
+quote: str = "I don't give up when I'm tired, I give up when I'm finish"
+
+print(f"David Goggins once said,{quote}")
