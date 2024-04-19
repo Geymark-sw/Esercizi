4b3b3e680b9a26b510d139d9ac348cb97998802b
Geymark-sw <emmanuelpapio12@gmail.com>
2024-04-19 13:34:47 +0200

    Esercizio 3 fatto

---
+++ lezione 4 Bhard/esercizio_funzioni_3.py
@@ -0,0 +1,13 @@
+def print_numbers(l:list) -> None:
+
+    for i in l:
+        eln = 1 # per stampare Elemento n°
+        print(f"Elemento n°{eln} della lista: {i}")
+
+        eln += 1
+
+
+
+l:list = [1,2,3]
+
+print_numbers(l)
