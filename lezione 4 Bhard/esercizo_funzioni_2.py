2729f912a97c966ef4dec0899a048be39b17fdc6
Geymark-sw <emmanuelpapio12@gmail.com>
2024-04-19 13:00:13 +0200

    fatto 2 fun

---
+++ lezione 4 Bhard/esercizo_funzioni_2.py
@@ -0,0 +1,11 @@
+def check_value(a):
+    if a > 5:
+        print(f"Il numero {a} è maggiore di 5")
+
+    elif a < 5:
+        print(f"Il numero {a} è maggiore di 5")
+
+    else:
+        print(f"Il numero {a} è uguale a 5")
+
+a: float = int(input("Inserisci il numero: "))
