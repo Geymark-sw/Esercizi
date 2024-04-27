8f60f7b52f4e98a46e0c4540e2937d68d78fa7bc
Geymark-sw <emmanuelpapio12@gmail.com>
2024-04-27 23:52:55 +0200

    modificato 3-6

---
+++ Lezione 2/3-6. More Guests.py
@@ -0,0 +1,23 @@
+"""
+3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.
+"""
+
+p: list[str] = ["Andrea Larosa","Abella Danger","Gaggi Yatarov"]
+
+for i in p:
+    print(f"Ciao {i}, ti andrebbe di uscire a cena?")
+    
+print(f"{p[2]}: Mi dispiace ma non posso venire a cena :(")
+
+p[2] = "Emanuele Majeli"
+
+for i in p:
+    print(f"Ciao {i}, ti andrebbe di uscire a cena?")
+
+print("RAGAZZI HO TROVATO UN TAVOLO PIÙ GRANDE, PROVO AD INVITARE ALTRE 3 PERSONE")
+
