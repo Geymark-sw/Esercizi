42b9b1c4f087995f6ebb2c4e21398786c0209288
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-15 23:22:44 +0200

    fatto 6-1 Lezione 2

---
+++ Lezione 2/6-1. Person.py
@@ -0,0 +1,16 @@
+dizionario: dict[str:object] = {"first_name":"Geymark Emmanuel", "last_name":"Papio", "age":20, "city": "Roma"}
+
+
+#stampo coppia chiave valore / oggetto
+for i in dizionario.items():
+    print(i)
+
+#stampo solo le chiavi / stringa
+for i in dizionario.keys():
+    print(i)
+
+#stampo solo i valori / stringa
+for i in dizionario.values():
+    print(i)
+
+    
