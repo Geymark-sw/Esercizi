06e34a49221a642355866210b2768397396b8915
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-15 23:34:14 +0200

    fatto 6-2 Lezione 2

---
+++ Lezione 2/6-2. Favorite Numbers.py
@@ -0,0 +1,11 @@
+dizionario: dict[str:int] = {"Gey":3, "Ronan":11, "Giuse":13, "Grace":10, "Mumy":29}
+
+for i in dizionario.items():
+    print(i)
+
+for i in dizionario.keys():
+
+    print(i)
+
+for i in dizionario.values():
+    print(i)
