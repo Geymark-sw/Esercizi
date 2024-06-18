e89fa00b45e2d85adaf9d114df0e2c2ab4ba3d0a
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-18 13:56:03 +0200

    Merge sort

---
+++ Lezione 19/Merge sort.py
@@ -0,0 +1,45 @@
+def mergeSort(a: list[int]) -> list[int]:
+
+    """
+    
+    """
+
+    if len(a) == 1:
+        return a 
+
+    mid_point: int = len(a) // 2
+
+    mergeSort(a[:mid_point])
+    mergeSort(a[mid_point:])
+
+
+def merge(list_1: list[int], list_2: list[int]) -> list[int]:
+
+    i, j = 0, 0
+
+    result: list[int] = [None for _ in range(len(list_1 + list_2))]
+
+    for k in range(len(result)):
+
+        if list_1[i] > list_2[j]:
+
+            result[k] = list_2[j]
+            j+=1
+        else:
+
+            result[k] = list_1[i]
+            i+=1
+
+
+
+
+            
+
+
+
+if __name__ == "__main__":
+
+    a: list[int] = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+    mergeSort(a)
+
