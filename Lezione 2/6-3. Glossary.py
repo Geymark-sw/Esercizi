5d4ca2cf7a7d2173e32ff2ae51d9547ea046dc17
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-20 22:48:39 +0200

    fatto 6-3 Lezione 2

---
+++ Lezione 2/6-3. Glossary.py
@@ -0,0 +1,14 @@
+glossary: dict[str:str] = {"if":"Struttura di selezione che esegue delle istruzioni in base alla condizione che può essere vera o falsa",
+                           
+                           "for":"Struttura di ciclo dove è possibile iterari attraverso una collections e/o eseguire una determinata serie di istruzioni contate attraverso il metodo range",
+                           
+                           "while":"Struttura di ciclo che itera attraverso una condizione",
+                           
+                           "range":"Funzione built-in in python che genera una serie di numeri.",
+                           
+                           "lista":"Collections indicizzata che può contenere qualsiasi tipo di dato"}
+
+
+for i in glossary.items():
+
+    print(i)
