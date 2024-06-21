ed127c800129089b81e5c8b8dd35227f9fc9056a
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-21 22:28:28 +0200

    fatto 6-8 e 6-9 lezione 2

---
+++ Lezione 2/6-8. Pets.py
@@ -0,0 +1,10 @@
+cane : dict[str:str] = {"tipo":"Cane","proprietario":"Grace"}
+gatto: dict[str:str] = {"tipo":"Gatto","proprietario":"Wilton"}
+pappagallo: dict[str:str] = {"tipo":"Pappagallo","proprietario":"Cristian"}
+capybara: dict[str:str] = {"tipo":"Capybare","proprietario":"Giuse"}
+
+lista: [dict] = [cane,gatto,pappagallo,capybara]
+
+for i in lista:
+
+    print(i)
---
+++ Lezione 2/6-9. Favorite Places.py
@@ -0,0 +1,5 @@
+favorite_places: dict[str:str] = {"Ronan":"Centro","Giuse":"Casa di Valeria","Cristian":"Stadio olimpico"}
+
+for i in favorite_places.keys():
+
+    print(f"Il posto preferito di {i} è: {favorite_places[i]}")
