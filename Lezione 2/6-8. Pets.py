f448574a239ce45a5553d308b6b6eff1a97c1d92
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-21 22:30:09 +0200

    ciao

--- Lezione 2/6-8. Pets.py
+++ Lezione 2/6-8. Pets.py
@@ -3,7 +3,7 @@ gatto: dict[str:str] = {"tipo":"Gatto","proprietario":"Wilton"}
 pappagallo: dict[str:str] = {"tipo":"Pappagallo","proprietario":"Cristian"}
 capybara: dict[str:str] = {"tipo":"Capybare","proprietario":"Giuse"}
 
-lista: [dict] = [cane,gatto,pappagallo,capybara]
+lista: [dict[str:str]] = [cane,gatto,pappagallo,capybara]
 
 for i in lista:
 
