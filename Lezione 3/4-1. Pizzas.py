b0916d8639169acc6c9867fece7b5f9835af26eb
Geymark-sw <emmanuelpapio12@gmail.com>
2024-06-22 17:34:44 +0200

    ciao

---
+++ Lezione 3/4-1. Pizzas.py
@@ -0,0 +1,15 @@
+lista_pizze: list[str] = ["Margherita","Boscaiola","Napoli"]
+
+
+#ciclo per stampare solo i contenuti della lista
+for i in lista_pizze:
+
+    print(i)
+
+#ciclo per stampare una frase con il contenuto della lista
+for i in lista_pizze:
+
+    print(f"Mi piace la pizza {i}")
+
+print("ADORO LE PIZZEEEE!!!!!")#cia9
+
