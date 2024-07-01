8dafcdf436af8dab43039b4b106ec93d2a191499
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-02 00:46:27 +0200

    fatto esercizione 4-4 e 4-5 lezione 3

---
+++ Lezione_3/4-4_One Million.py
@@ -0,0 +1,4 @@
+milione: list[int] = [i for i in range(1,1000001)]
+for i in milione:
+
+    print(i)
---
+++ Lezione_3/4-5. Summing a Million.py
@@ -0,0 +1,6 @@
+milione: list[int] = [i for i in range(1,1000001)]
+
+print(min(milione))
+print(max(milione))
+
+print(sum(milione))
