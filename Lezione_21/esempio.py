36ed6e86df2376b9e229e37c26efec29988854b2
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-03 15:55:45 +0200

    Threads

--- Lezione_21/esempio.py
+++ Lezione_21/esempio.py
@@ -43,7 +43,25 @@ def thread_function(name):
 
     print(f"{name} Time - {time.time()}")
 
+
 import threading
+import random
+
+def funzione(id: int):
+    sleep_time: int = random.randint(1,10)
+    print(f"{id=} time {time.time()}")
+    time.sleep(sleep_time)
+    print(f"{id=} time {time.time()}")
+
+if __name__ == "__main__":
+    
+    from concurrent.futures import ThreadPoolExecutor
+
+    with ThreadPoolExecutor(max_workers=10) as esecutor:
+    
+        esecutor.map(funzione, range(100))
+
+
 
 for i in range(3):
 
