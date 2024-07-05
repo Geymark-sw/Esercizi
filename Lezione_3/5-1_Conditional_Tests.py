7c1aa7e98ecac9bf83311e103f2c6ae157cb0763
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-05 21:45:00 +0200

    fatto 5-1 Lezione 3

---
+++ Lezione_3/5-1_Conditional_Tests.py
@@ -0,0 +1,36 @@
+#1 True
+car: str = "Subaru"
+print("Is car == Subaru?")
+print(car == "Subaru")
+#2 True
+car = "Ferrari"
+print("Is car == Ferrari?")
+print(car == "Ferrari")
+#3 True
+car = "Lamborghini"
+print("Is car == Lamborghini?")
+print(car == "Lamborghini")
+#4 True
+car = "Pagani"
+print("Is car == Pagani?")
+print(car == "Pagani")
+#5
+car = "Koenigsegg"
+print("Is car == Koenigsegg")
+print(car == "Koenigsegg")
+#6
+car = "Subaru"
+print("Is car == Audi?")
+print(car == "Audi")
+#7
+print("Is car == Toyota?")
+print(car == "Toyota")
+#8
+print("Is car == Nissan?")
+print(car == "Nissan")
+#9
+print("Is car == Alfa Romeo?")
+print(car == "Alfa Romeo")
+#10
+print("Is car == BMW?")
+print(car == "BMW")
