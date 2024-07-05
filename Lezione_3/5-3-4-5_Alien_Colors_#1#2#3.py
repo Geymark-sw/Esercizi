7f4275cc32be3513ba441dc3ed403f1c14806146
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-05 21:59:46 +0200

    5 -3-4-5 Lezione 3

---
+++ Lezione_3/5-3-4-5_Alien_Colors_#1#2#3.py
@@ -0,0 +1,20 @@
+alien_color: str = "niggets"
+
+if alien_color == "green":
+    print("Wow you earned 5 points!!!!")
+
+else:
+    print("Wow HAI GUADAGNATO 10 PUNTI!!!!!")
+
+
+
+alien_color = "niggets"
+
+if alien_color == "green":
+    print("Wow you earned 5 points!!!!")
+
+elif alien_color == "yellow":
+    print("Wow HAI GUADAGNATO 10 PUNTI!!!!!")
+
+elif alien_color == "red":
+    print("EDDAAJEEE HAI GUADAGNATO 15 PUNTI FRATEEEE CHE HAI BECCATO L'ALIENO ROSSOOOOOO!!!!")
