90f34096477ce03f670acb0e31470355472b214f
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-14 13:33:39 +0200

    User - lezione 7

--- Lezione_6/9-3_Users.py
+++ Lezione_6/9-3_Users.py
@@ -10,12 +10,23 @@ class User:
         self.seguiti: int = seguiti
 
 
-    def describe_user(self) -> str:
+    def describe_user(self) -> None:
 
-        print(f"{self.__class__.__name__}\n"
-        f"First name: {self.first_name} L")
+        print(f"{self.__class__.__name__}:\n"
+        f"First name: {self.first_name}\n"
+        f"Last name: {self.last_name}\n"
+        f"Età: {self.età}\n"
+        f"Genere: {self.genere}\n"
+        f"Follower: {self.follower}\n"
+        f"Seguiti: {self.seguiti}\n")
 
+    def greet_user(self) -> None:
+        print(f"Grazie per aver effettuato l'accesso {self.first_name} {self.last_name}\n")
 
 a: User = User("Geymark Emmanuel","Papio",20,"M",20,20)
+b: User = User("Grace Mackenzie","Papio",18,"F",10,10)
 
 a.describe_user()
+a.greet_user()
+b.describe_user()
+b.greet_user()
