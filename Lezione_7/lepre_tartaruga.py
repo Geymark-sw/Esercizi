1bab36c47155443d6ba446cdb18a2d99661ae7dd
Geymark-sw <emmanuelpapio12@gmail.com>
2024-07-15 19:27:14 +0200

    lepre tartaruga forse finito

--- Lezione_7/lepre_tartaruga.py
+++ Lezione_7/lepre_tartaruga.py
@@ -4,20 +4,30 @@ from random import randint
 
 
 
-#funzione che prende in ingresso una lista di posizioni e la restituisce aggiornata
-def calcola_tartaruga(lista_posizioni: list[str],posizione: int) -> list[str]:
+#funzione che prende in ingresso una lista di posizioni e la restituisce aggiornata con la posizione aggiornata della tatrtaruga
+def calcola_tartaruga(lista_posizioni: list[str],posizione: int) -> dict:
 
     num: int = randint(1,10)
+    spostamento: int = 0
 
 
     #passo veloce: avanza di 3 posizioni
     if num >= 1 and num <= 5: 
-        if lista_posizioni[posizione + 3] == "_":
+
+        if posizione + 3 >= len(lista_posizioni) - 1:
+
+            lista_posizioni[len(lista_posizioni)-1] = "Tartaruga"
+
+        elif lista_posizioni[posizione + 3] == "_":
             lista_posizioni[posizione + 3] = "Tartaruga"
             
         else:
 
             lista_posizioni[posizione + 3] = "OUCH"
+        
+        posizione += 3
+        spostamento = 3
+        
         
         
 
@@ -29,41 +39,74 @@ def calcola_tartaruga(lista_posizioni: list[str],posizione: int) -> list[str]:
                 lista_posizioni[0] = "Tartaruga"
             else:
                 lista_posizioni[0] = "OUCH"
+            spostamento = -posizione
+            posizione = 0
+            
 
 
         elif lista_posizioni[posizione - 6] == "_":
-                lista_posizioni[posizione - 6] = "Tartaruga"
-
+            lista_posizioni[posizione - 6] = "Tartaruga"
+            posizione -= 6
+            spostamento = -6
         else:
             lista_posizioni[posizione - 6] = "OUCH"
+            posizione -= 6
+            spostamento = -6
 
         
 
     #passo lento: avanza di 1 posizione
+
+    
+    elif posizione + 1 >= len(lista_posizioni) - 1:
+
+            lista_posizioni[len(lista_posizioni)-1] = "Tartaruga"
+
     elif lista_posizioni[posizione + 1] == "_":
 
         lista_posizioni[posizione + 1] = "Tartaruga"
+        spostamento = 1
+        posizione += 1
     else:
         lista_posizioni[posizione + 1] = "OUCH"
+        spostamento = 1
+        posizione += 1
+
+    #controllo posizione precedente
+    if lista_posizioni[posizione - spostamento] == "OUCH":
+
+        lista_posizioni[posizione - spostamento] = "Lepre"
+
+    else:
+
+        lista_posizioni[posizione - spostamento] = "_"
     
-    lista_posizioni[posizione] = "_"
 
 
-    return lista_posizioni
+    return {"lista":lista_posizioni, "posizione":posizione}
 
-def calcola_lepre(lista_posizioni: list[str], posizione: int) -> list[str]:
+def calcola_lepre(lista_posizioni: list[str], posizione: int) -> dict:
 
     num: int = randint(1,10)
+    spostamento: int = 0
 
     #riposo
     if num >= 1 and num <= 2:
         pass
     #grande balzo: avanza di 9 quadrati
     elif num >= 3 and num <= 4:
-        if lista_posizioni[posizione + 9] == "_":
+
+        if posizione + 9 >= len(lista_posizioni) - 1:
+
+            lista_posizioni[len(lista_posizioni)-1] = "Lepre"
+
+        elif lista_posizioni[posizione + 9] == "_":
             lista_posizioni[posizione + 9] = "Lepre"
         else:
             lista_posizioni[posizione + 9] = "OUCH"
+        posizione += 9
+        spostamento = 9
+        
     #grande scivolata: arretra di 12 posizioni ma non può andare sotto la posizione 1
     elif num == 5:
 
@@ -73,18 +116,33 @@ def calcola_lepre(lista_posizioni: list[str], posizione: int) -> list[str]:
 
             else:
                 lista_posizioni[0] = "OUCH"
+            
+            spostamento = -posizione
+            posizione = 0
 
         elif lista_posizioni[posizione - 12] == "_":
             lista_posizioni[posizione - 12] = "Lepre"
+            posizione -= 12
+            spostamento = -12
         else:
             lista_posizioni[posizione - 12] = "OUCH"
+            posizione -= 12
+            spostamento = -12
+
+        
     #piccolo balzo: avanza di 1 posizione
     elif num >= 6 and num <= 8:
+
+        if posizione + 1 >= len(lista_posizioni) - 1:
+
+            lista_posizioni[len(lista_posizioni)-1] = "Lepre"
 
         if lista_posizioni[posizione + 1] == "_":
             lista_posizioni[posizione + 1] = "Lepre"
         else:
             lista_posizioni[posizione + 1] = "OUCH"
+        posizione += 1
+        spostamento = 1
     #piccola scivolata: arretra di 2 posizioni ma non può andare sotto la posizione 1
     else:
 
@@ -93,27 +151,52 @@ def calcola_lepre(lista_posizioni: list[str], posizione: int) -> list[str]:
                 lista_posizioni[0] = "Lepre"
             else:
                 lista_posizioni[0] = "OUCH"
+            spostamento = -posizione
+            posizione = 0
 
         elif lista_posizioni[posizione - 2] == "_":
             lista_posizioni[posizione - 2] = "Lepre"
+            
 
         else:
             lista_posizioni[posizione - 2] = "OUCH"
 
-    lista_posizioni[posizione] = "_"
+        posizione -= 2
+        spostamento = -2
+
+    #cambio lo stato della vecchia posizione
+    if lista_posizioni[posizione - spostamento] == "OUCH":
+
+        lista_posizioni[posizione - spostamento] = "Tartaruga"
+
+    elif num >= 1 and num <= 2:#se la lepre ha riposato, deve rimanere nella sua posizione, non deve scomparire :)
+        pass
+    else:
+
+        lista_posizioni[posizione - spostamento] = "_"
 
 
-    return lista_posizioni
+    return {"lista":lista_posizioni, "posizione":posizione}
 
 
 lista_posizioni: list[str] = ["_" for i in range(70)]
+posizione: int = 0
 
-for i in range(len(lista_posizioni)):
+tartaruga: dict = {"lista":lista_posizioni,"posizione":posizione}
+lepre: dict = {"lista":lista_posizioni,"posizione":posizione}
 
-    lista_posizioni = calcola_tartaruga(lista_posizioni,i)
-    lista_posizioni = calcola_lepre(lista_posizioni,i)
 
-    print(lista_posizioni)
+
+while lista_posizioni[len(lista_posizioni) - 1] == "_":
+
+    tartaruga = calcola_tartaruga(tartaruga["lista"],tartaruga["posizione"])
+    lepre["lista"] = tartaruga["lista"]
+    lepre = calcola_lepre(lepre["lista"],lepre["posizione"])
+    tartaruga["lista"] = lepre["lista"]
+
+    print(lepre["lista"])
+    print("\n\n")
+
 
     time.sleep(0.5)
     
